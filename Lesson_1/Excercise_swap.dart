f50db4af2af7656c98d687144b7fb9e890d9838f
tungcuong <tungcuong131@gmail.com>
2024-01-31 12:45:40 +0700

    add exercise swap

--- Lesson_1/Excercise_swap.dart
+++ Lesson_1/Excercise_swap.dart
@@ -7,6 +7,9 @@ void main() {
       '${identityHashCode(phones[0])} - ${identityHashCode(phones[1])} - ${identityHashCode(phones[2])}');
 
   // viết code ở đây, đổi vị trí Xiaomi và iPhone
+  phones[1] = phones[1]+ phones[2];
+  phones[2] = phones[1].replaceAll(phones[2], "");
+  phones[1] = phones[1].replaceAll(phones[2], "");
   print(phones);
   print(
       '${identityHashCode(phones[0])} - ${identityHashCode(phones[1])} - ${identityHashCode(phones[2])}');
